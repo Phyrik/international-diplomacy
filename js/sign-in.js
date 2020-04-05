9ed87b3349f3c51d8e34baa1958f59af874007c0
Phyrik <34688504+Phyrik@users.noreply.github.com>
2020-04-05 22:07:00 +0100

    google auth2 sign out

--- js/sign-in.js
+++ js/sign-in.js
@@ -1,5 +1,17 @@
 function onGoogleSignIn(googleUser) {
+    const urlParams = new URLSearchParams(window.location.search)
+    const signOutParam = urlParams.get("sign-out")
+    if (signOutParam == "true") {
+        googleSignOut()
+        return
+    }
     var Gprofile = googleUser.getBasicProfile()
     localStorage.setItem("GUserEmail", Gprofile.getEmail())
     window.location.href = "./game.html"
+}
+
+function googleSignOut() {
+    localStorage.removeItem("GUserEmail")
+    var auth2 = gapi.auth2.getAuthInstance()
+    auth2.signOut()
 }
