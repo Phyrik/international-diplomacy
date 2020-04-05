6d19c32495b719e187dfff2ccf952143db06615f
Phyrik <34688504+Phyrik@users.noreply.github.com>
2020-04-05 21:51:05 +0100

    sign-in redirect

--- js/sign-in.js
+++ js/sign-in.js
@@ -7,4 +7,5 @@ function googleSignOut() {
 function onGoogleSignIn(googleUser) {
     var Gprofile = googleUser.getBasicProfile()
     localStorage.setItem("GUserEmail", Gprofile.getEmail())
+    window.location.href = "./game.html"
 }
